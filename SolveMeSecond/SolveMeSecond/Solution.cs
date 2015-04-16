e87d4f4919ce5b16a8df42d2c10cafb8ce55eca9
Nicholas Costello <nicholasjcostello@gmail.com>
2015-04-15 23:12:55 -0400

    Utopian Tree Solution

--- SolveMeSecond/SolveMeSecond/Solution.cs
+++ SolveMeSecond/SolveMeSecond/Solution.cs
@@ -2,39 +2,34 @@
 
 using System;
 
-namespace SolveMeSecond
+internal class Solution
 {
-    internal class Solution
+    private static void Main(string[] args)
     {
-        private static void Main(string[] args)
+        //Find the number of inputs expected
+        int numberOfExpectedInputs;
+        int.TryParse(Console.ReadLine(), out numberOfExpectedInputs);
+
+        for (var i = 0; i < numberOfExpectedInputs; ++i)
         {
-            //Find the number of inputs expected
-            int numberOfExpectedInputs;
-            int.TryParse(Console.ReadLine(), out numberOfExpectedInputs);
+            //Read and split the string
+            var input = Console.ReadLine();
+            var splitInput = input.Split(' ');
 
-            for (var i = 0; i < numberOfExpectedInputs; ++i)
-            {
-                //Read and split the string
-                var input = Console.ReadLine();
-                string[] splitInput = input.Split(' ');
+            //Parse the integers
+            int constant1;
+            int constant2;
+            if (!int.TryParse(splitInput[0], out constant1)) break;
+            if (!int.TryParse(splitInput[1], out constant2)) break;
 
-                //Parse the integers
-                int constant1;
-                int constant2;
-                if (!int.TryParse(splitInput[0], out constant1)) break;
-                if (!int.TryParse(splitInput[1], out constant2)) break;
-                
-                //Calculate the sum
-                Console.WriteLine(ComputSum(constant1,constant2));
-            }
-
-
+            //Calculate the sum
+            Console.WriteLine(ComputeSum(constant1, constant2));
         }
+    }
 
-        //Simply computes a sum
-        private static int ComputSum(int a, int b)
-        {
-            return a + b;
-        }
+    //Simply computes a sum
+    private static int ComputeSum(int a, int b)
+    {
+        return a + b;
     }
 }
---
+++ UtopianTree/UtopianTree/Solution.cs
@@ -0,0 +1,28 @@
+﻿// Copyright 2015 Nicholas Costello <NicholasJCostello@gmail.com>
+
+using System;
+
+internal class Solution
+{
+    private static void Main(string[] args)
+    {
+        //Get the number of test cases
+        int numberOfTestCases;
+        if (!int.TryParse(Console.ReadLine(), out numberOfTestCases)) return;
+
+        //Loop through each test case
+        for (var i = 0; i < numberOfTestCases; i++)
+        {
+            //Get the number of cycles
+            int numberOfCycles;
+            if (!int.TryParse(Console.ReadLine(), out numberOfCycles)) break;
+
+            //Calculate the height depending on the cycles
+            var height = 0;
+            for (var j = 0; j <= numberOfCycles; j++)
+                height += j%2 == 0 ? 1 : height;
+
+            Console.WriteLine(height);
+        }
+    }
+}
