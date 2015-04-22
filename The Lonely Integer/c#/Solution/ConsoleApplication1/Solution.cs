2748bbf9fafa287142ed7f4c5c19bdb2b1528f7a
Nicholas Costello <nicholasjcostello@gmail.com>
2015-04-21 21:30:43 -0400

    Added and complete the lonely integer problem. Added unit tests for the hell of it.

---
+++ The Lonely Integer/c#/Solution/ConsoleApplication1/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    private static void Main(string[] args)
+    {
+        //Read 
+        var input = Console.ReadLine();
+        int integerCount;
+        int.TryParse(input, out integerCount);
+
+        //Gather integers
+        var integerInput = Console.ReadLine().Split(' ');
+        var integers = new int[integerCount];
+        for (int i = 0; i < integerCount; i++)
+        {
+            integers[i] = int.Parse(integerInput[i]);
+        }
+
+        //Find lonel integers
+        Console.WriteLine(FindLonelyInteger(integers));
+    }
+
+
+    public static int FindLonelyInteger(int[] ints)
+    {
+        var values = new List<int>();
+        for (int i = 0; i < ints.Length; i++)
+        {
+            if (values.Contains(ints[i]))
+            {
+                values.Remove(ints[i]);
+            }
+            else
+            {
+                values.Add(ints[i]);
+            }
+        }
+        return values[0];
+    }
+
+}
---
+++ The Lonely Integer/c#/Solution/UnitTests/LonelyIntegerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace UnitTests
+{
+    [TestClass]
+    public class LonelyIntegerTests
+    {
+        [TestMethod]
+        public void LonelyIntegerTest1 ()
+        {
+            var testInts = new int[1]{5};
+
+            var test = Solution.FindLonelyInteger(testInts);
+
+            Assert.AreEqual(5,test);
+        }
+
+        [TestMethod]
+        public void LonelyIntegerTest2()
+        {
+            var testInts = new int[5] { 10, 12, 10, 11, 12};
+
+            var test = Solution.FindLonelyInteger(testInts);
+
+            Assert.AreEqual(11, test);
+            Assert.AreNotEqual(10, test);
+            Assert.AreNotEqual(12, test);
+        }
+    }
+}
