5507b2879199b8109f8476b2255fffee2d08f4e5
Nicholas Costello <nicholasjcostello@gmail.com>
2015-04-22 10:33:29 -0400

    First brute force, recursive, solution to cut the sticks

---
+++ Cut-The-Sticks/Cut The Sticks/Cut The Sticks/Solution.cs
@@ -0,0 +1,51 @@
+﻿// Copyright 2015 Nicholas Costello <NicholasJCostello@gmail.com>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    private static void Main(string[] args)
+    {
+        //Read and split the string
+        var inputTestCases = Console.ReadLine();
+        if (inputTestCases == null) return;
+
+        //Parse input
+        var numberOfTestCase = 0;
+        int.TryParse(inputTestCases, out numberOfTestCase);
+
+        var inputIntegers = Console.ReadLine().Split(' ');
+
+        var values = new List<int>();
+        for (var i = 0; i < numberOfTestCase; i++)
+            values.Add(int.Parse(inputIntegers[i]));
+
+        CutIntegers(values);
+    }
+
+    private static void CutIntegers(List<int> values)
+    {
+        int numberOfCuts = 0;
+
+        if (values.Count == 0)
+            return;
+
+        var min = values.Min();
+
+        for (var i = 0; i < values.Count; i++)
+        {
+            values[i] -= min;
+            if (values[i] <= 0)
+            {
+                values.Remove(values[i]);
+                i--;
+            }
+
+            numberOfCuts++;
+        }
+        Console.WriteLine(numberOfCuts);
+        CutIntegers(values);
+    }
+}
